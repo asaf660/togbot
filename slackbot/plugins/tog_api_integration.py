2352b8b20872405bb4d54974816d07ae68697b25
Omer Hamerman <omerxx@gmail.com>
2017-01-27 16:33:58 +0200

    using utils fetch conf for slackbot settings

--- slackbot/plugins/tog_api_integration.py
+++ slackbot/plugins/tog_api_integration.py
@@ -1,8 +1,14 @@
-Create new toggl object (if previously not created - e.g toggl_omer)
-setAPIKey - if not in conf set im PM
-startTimeEntry
-currentRunningTimeEntry
-stopTimeEntry
+# Create new toggl object (if previously not created - e.g toggl_omer)
+# setAPIKey - if not in conf set im PM
+# startTimeEntry
+# currentRunningTimeEntry
+# stopTimeEntry
 
-createTimeEntry
-randomizeTimeEntry
+# createTimeEntry
+# randomizeTimeEntry
+
+from slackbot.bot import respond_to
+
+@respond_to('$set (.*) hours .* working on (.*)')
+def entry(message, hours, project):
+    print 'Hours: {}, Project: {}'.format(hours, project)
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,16 +1,7 @@
 import yaml
 import os
+from slackbot.plugins.utils import fetch_conf
 
-def fetch_conf():
-    dataMap = {}
-    try:
-        with open('conf.yml') as conf:
-            dataMap = yaml.safe_load(conf)
-    except:
-        dataMap['TOGGL_API_TOKEN'] = os.environ['TOGGL_API_TOKEN']
-        dataMap['SLACK_API_TOKEN'] = os.environ['SLACK_API_TOKEN']
-
-    return dataMap
 
 API_TOKEN = fetch_conf().get('SLACK_API_TOKEN')
 DEFAULT_REPLY = "YO!"
